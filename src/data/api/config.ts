1457312f9b0b1828b69e1038c3649aa855eae1df
manish23 <manishdoley23@gmail.com>
2025-01-23 20:58:44 +0530

    chore: increase timeout time

--- src/data/api/config.ts
+++ src/data/api/config.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
-const API_TIMEOUT = import.meta.env.VITE_API_TIMEOUT || 10000;
+const API_TIMEOUT = import.meta.env.VITE_API_TIMEOUT || 100000;
 
 export const api: AxiosInstance = axios.create({
   baseURL: `${API_BASE_URL}/api/`,
